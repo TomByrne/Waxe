180c653a5bac5e73172371bc56017c6d518ab237
gamehaxe <gamehaxe@ec372be2-0989-11df-b76f-33ad6b24dacb>
2010-01-28 13:29:57 +0000

    Add brush & font
    git-svn-id: http://waxe.googlecode.com/svn/trunk@9 ec372be2-0989-11df-b76f-33ad6b24dacb

--- src/DC.cpp
+++ src/DC.cpp
@@ -23,6 +23,54 @@ value wx_dc_draw_line(value inDC, value inX1, value inY1, value inX2, value inY2
 }
 DEFINE_PRIM(wx_dc_draw_line,5)
 
+value wx_dc_draw_rectangle(value inDC, value x, value y, value w, value h)
+{
+	wxDC *dc;
+	if (ValueToWX(inDC,dc))
+	{
+		dc->DrawRectangle(val_int(x), val_int(y), val_int(w), val_int(h) );
+	}
+	return alloc_null();
+}
+DEFINE_PRIM(wx_dc_draw_rectangle,5)
+
+
+value wx_dc_draw_ellipse(value inDC, value x, value y, value w, value h)
+{
+	wxDC *dc;
+	if (ValueToWX(inDC,dc))
+	{
+		dc->DrawEllipse(val_int(x), val_int(y), val_int(w), val_int(h) );
+	}
+	return alloc_null();
+}
+DEFINE_PRIM(wx_dc_draw_ellipse,5)
+
+value wx_dc_draw_circle(value inDC, value x, value y, value rad)
+{
+	wxDC *dc;
+	if (ValueToWX(inDC,dc))
+	{
+		dc->DrawCircle(val_int(x), val_int(y), val_int(rad) );
+	}
+	return alloc_null();
+}
+DEFINE_PRIM(wx_dc_draw_circle,4)
+
+value wx_dc_draw_text(value inDC, value text, value x, value y)
+{
+	wxDC *dc;
+	if (ValueToWX(inDC,dc))
+	{
+		dc->DrawText(Val2Str(text), val_int(x), val_int(y) );
+	}
+	return alloc_null();
+}
+DEFINE_PRIM(wx_dc_draw_text,4)
+
+
+
+
 value wx_dc_clear(value inDC)
 {
 	wxDC *dc;
@@ -41,5 +89,28 @@ value wx_dc_set_pen(value inDC,value inPen)
 	return alloc_null();
 }
 DEFINE_PRIM(wx_dc_set_pen,2)
+
+
+value wx_dc_set_font(value inDC,value inFont)
+{
+	wxDC *dc;
+	wxFont *font;
+	if (ValueToWX(inDC,dc) && ValueToWX(inFont,font) )
+      dc->SetFont(*font);
+	return alloc_null();
+}
+DEFINE_PRIM(wx_dc_set_font,2)
+
+value wx_dc_set_brush(value inDC,value inBrush)
+{
+	wxDC *dc;
+	wxBrush *brush;
+	if (ValueToWX(inDC,dc) && ValueToWX(inBrush,brush) )
+      dc->SetBrush(*brush);
+	return alloc_null();
+}
+DEFINE_PRIM(wx_dc_set_brush,2)
+
+
 
 
--- wx/DC.hx
+++ wx/DC.hx
@@ -2,37 +2,71 @@ package wx;
 
 class DC
 {
-	public var pen(null,setPen):Pen;
-	var wxHandle:Dynamic;
+   public var pen(null,setPen):Pen;
+   public var brush(null,setBrush):Brush;
+   public var font(null,setFont):Font;
+   var wxHandle:Dynamic;
 
-	function new(handle:Dynamic)
-	{
-		wxHandle = handle;
-	}
-	public function destroy() { wx_object_destroy(wxHandle); }
+   function new(handle:Dynamic)
+   {
+      wxHandle = handle;
+   }
+   public function destroy() { wx_object_destroy(wxHandle); }
 
-	public function drawLine(x1:Int, y1:Int, x2:Int, y2:Int)
-	{
-		wx_dc_draw_line(wxHandle,x1,y1,x2,y2);
-	}
+   public function drawLine(x1:Int, y1:Int, x2:Int, y2:Int)
+   {
+      wx_dc_draw_line(wxHandle,x1,y1,x2,y2);
+   }
    public function clear()
    {
       wx_dc_clear(wxHandle);
    }
-	public function setPen(inPen:Pen) : Pen
-	{
-		wx_dc_set_pen(wxHandle,inPen.wxGetHandle());
-		return inPen;
-	}
+   public function setPen(inPen:Pen) : Pen
+   {
+      wx_dc_set_pen(wxHandle,inPen.wxGetHandle());
+      return inPen;
+   }
+   public function setBrush(inBrush:Brush) : Brush
+   {
+      wx_dc_set_brush(wxHandle,inBrush.wxGetHandle());
+      return inBrush;
+   }
+   public function setFont(inFont:Font) : Font
+   {
+      wx_dc_set_font(wxHandle,inFont.wxGetHandle());
+      return inFont;
+   }
+   public function drawRectangle(x:Int, y:Int, width:Int, height:Int)
+   {
+      wx_dc_draw_rectangle(wxHandle,x,y,width,height);
+   }
+   public function drawCircle(x:Int, y:Int, rad:Int)
+   {
+      wx_dc_draw_circle(wxHandle,x,y,rad);
+   }
+   public function drawEllipse(x:Int, y:Int, width:Int, height:Int)
+   {
+      wx_dc_draw_ellipse(wxHandle,x,y,width,height);
+   }
+   public function drawText(text:String,x:Int, y:Int)
+   {
+      wx_dc_draw_text(wxHandle,text,x,y);
+   }
 
-	static public function createPaintDC(inWindow:Window)
-	{
-		return new DC(wx_dc_create_paint(inWindow.wxGetHandle()));
-	}
+   static public function createPaintDC(inWindow:Window)
+   {
+      return new DC(wx_dc_create_paint(inWindow.wxGetHandle()));
+   }
 
-	static var wx_dc_create_paint = neko.Lib.load("waxe","wx_dc_create_paint",1);
-	static var wx_dc_draw_line = neko.Lib.load("waxe","wx_dc_draw_line",5);
-	static var wx_object_destroy = neko.Lib.load("waxe","wx_object_destroy",1);
-	static var wx_dc_clear = neko.Lib.load("waxe","wx_dc_clear",1);
-	static var wx_dc_set_pen = neko.Lib.load("waxe","wx_dc_set_pen",2);
+   static var wx_dc_create_paint = neko.Lib.load("waxe","wx_dc_create_paint",1);
+   static var wx_dc_draw_line = neko.Lib.load("waxe","wx_dc_draw_line",5);
+   static var wx_object_destroy = neko.Lib.load("waxe","wx_object_destroy",1);
+   static var wx_dc_clear = neko.Lib.load("waxe","wx_dc_clear",1);
+   static var wx_dc_set_pen = neko.Lib.load("waxe","wx_dc_set_pen",2);
+   static var wx_dc_set_font = neko.Lib.load("waxe","wx_dc_set_font",2);
+   static var wx_dc_set_brush = neko.Lib.load("waxe","wx_dc_set_brush",2);
+   static var wx_dc_draw_rectangle = neko.Lib.load("waxe","wx_dc_draw_rectangle",5);
+   static var wx_dc_draw_circle = neko.Lib.load("waxe","wx_dc_draw_circle",4);
+   static var wx_dc_draw_ellipse = neko.Lib.load("waxe","wx_dc_draw_ellipse",5);
+   static var wx_dc_draw_text = neko.Lib.load("waxe","wx_dc_draw_text",4);
 }
